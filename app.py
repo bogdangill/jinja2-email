89f915720e756d036250d71f79170be70fa97241
Bogdan <herecomesthefog@gmail.com>
2026-03-26 18:30:09 +0500

    insert text content into json for local dev

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+with open('texts.json', 'r', encoding='utf-8') as f:
+    all_texts = json.load(f)
+
 @app.route('/')
 def home():
     # Sending data into template
-    return render_template('template.html')
+    return render_template('template.html', **all_texts)
 
 if __name__ == '__main__':
     # Run server locally
