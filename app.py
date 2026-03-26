9f8c76a2666d732ac2d2f2aaa0ea56bb98c9cded
Bogdan <herecomesthefog@gmail.com>
2026-03-26 19:41:36 +0500

    add livereload

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import json
 from flask import Flask, render_template
+from livereload import Server
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 with open('texts.json', 'r', encoding='utf-8') as f:
     all_texts = json.load(f)
@@ -12,5 +14,12 @@ def home():
     return render_template('template.html', **all_texts)
 
 if __name__ == '__main__':
-    # Run server locally
-    app.run(debug=True)
+    # create livereload server
+    server = Server(app.wsgi_app)
+
+    # point files & folders to watch for
+    server.watch('templates/')
+    server.watch('texts.json')
+
+    # http://127.0.0.1:5500 as default
+    server.serve(port=5500, debug=True)
